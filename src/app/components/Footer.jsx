ab296272595ed76d92e1f95a99a4397f4b9dc6a1
Whitney <whitneyannete254@gmail>
2024-08-26 08:59:08 +0300

    Edits

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container px-12 py-8 flex justify-between">
+      <div className="container px-12 py-8 flex justify-start">
       <img src="/images/Logo.png" alt="Logo" className = "h-16 w-auto"/>
-        <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
   );
